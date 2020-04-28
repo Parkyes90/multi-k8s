b358661fbb4bbdb5136b6086c5c4d216f277e915
parkyes90 <parkyes90@gmail.com>
2020-04-28 13:14:56 +0900

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t parkyes90/multi-client:latest -t parkyes90/multi-client:"$SHA" -f ./client/Dockerfile ./client
-docker build -t parkyes90/multi-server:latest -t parkyes90/multi-server:"$SHA" -f ./server/Dockerfile ./server
-docker build -t parkyes90/multi-worker:latest -t parkyes90/multi-worker:"$SHA" -f ./worker/Dockerfile ./worker
+docker build -t parkyes90/multi-client:latest -t parkyes90/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t parkyes90/multi-server:latest -t parkyes90/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t parkyes90/multi-worker:latest -t parkyes90/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push parkyes90/multi-client:latest
 docker push parkyes90/multi-server:latest
 docker push parkyes90/multi-worker:latest
 
-docker push parkyes90/multi-client:"$SHA"
-docker push parkyes90/multi-server:"$SHA"
-docker push parkyes90/multi-worker:"$SHA"
+docker push parkyes90/multi-client:$SHA
+docker push parkyes90/multi-server:$SHA
+docker push parkyes90/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=parkyes90/multi-server:"$SHA"
-kubectl set image deployments/client-deployment client=parkyes90/multi-client:"$SHA"
-kubectl set image deployments/worker-deployment worker=parkyes90/multi-worker:"$SHA"
+kubectl set image deployments/server-deployment server=parkyes90/multi-server:$SHA
+kubectl set image deployments/client-deployment client=parkyes90/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=parkyes90/multi-worker:$SHA
